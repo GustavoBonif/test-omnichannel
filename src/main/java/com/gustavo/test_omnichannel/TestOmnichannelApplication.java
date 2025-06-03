8be41b09e4c0451582d31accffb650e4880bc633
GustavoBonif <gubonic@gmail.com>
2025-06-02 22:55:43 -0300

    feat: initial setup

---
+++ src/main/java/com/gustavo/test_omnichannel/TestOmnichannelApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.test_omnichannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOmnichannelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOmnichannelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/test_omnichannel/TestOmnichannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.test_omnichannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOmnichannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
